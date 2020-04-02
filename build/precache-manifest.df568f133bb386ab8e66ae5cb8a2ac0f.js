2fe938d1b1ad7c6cba77ba0a320273f94d747f9a
garrettvercoe <garrettvercoe@gmail.com>
2020-04-02 16:01:44 -0400

    netlify work

---
+++ build/precache-manifest.df568f133bb386ab8e66ae5cb8a2ac0f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2766f66773bb959835a33ea129027c6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4038f5ceb5c72d13807",
+    "url": "/static/css/main.ff96e8b7.chunk.css"
+  },
+  {
+    "revision": "420671dae48a497fc4e1",
+    "url": "/static/js/2.12904c7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.12904c7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4038f5ceb5c72d13807",
+    "url": "/static/js/main.97ce5014.chunk.js"
+  },
+  {
+    "revision": "6206e974cf4e6c56fc05",
+    "url": "/static/js/runtime-main.420ef8c1.js"
+  },
+  {
+    "revision": "c09e9dbef88e67f8d6f44be1f433c2d2",
+    "url": "/static/media/Colfax-Medium.c09e9dbe.otf"
+  },
+  {
+    "revision": "bd0cad700b540d425f6618446629dd36",
+    "url": "/static/media/Colfax-Regular.bd0cad70.otf"
+  }
+]);
